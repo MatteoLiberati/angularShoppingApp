fac193d74122599ef2eb0d21343b8f7d15141716
MatteoLiberati <matteoliberati@hotmail.com>
2021-10-14 16:06:15 +0200

    ngfor recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes : [] = [];
-  
-  constructor() { }
+  recipes : Recipe[] = [
+    new Recipe("Roast Chicken", "with Chile-Basil Vinaigrette, Broccoli, and Potatoes", "https://cdn.pixabay.com/photo/2015/03/26/09/39/fried-chicken-690039_960_720.jpg"),
+    new Recipe("Asparagus", "Asparagus With Lemon-Pepper Marinade From Bryant Terry", "https://cdn.pixabay.com/photo/2016/04/04/17/22/meal-1307604_960_720.jpg"),
+    new Recipe("Beans", "Better Baked Beans", "https://cdn.pixabay.com/photo/2018/09/25/20/09/bush-beans-3702999_960_720.jpg"),
+  ];
+
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
