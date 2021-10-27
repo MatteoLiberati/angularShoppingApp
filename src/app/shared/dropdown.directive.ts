7880f4c9b89eb09d96770ec5bc9c60381d02ea09
MatteoLiberati <matteoliberati@hotmail.com>
2021-10-27 15:22:48 +0200

    better dropdown with closed by a click anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,10 +6,15 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen: boolean = false;
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
-  @HostListener('click') toogleOpen(){
-    this.isOpen = !this.isOpen;
+  // @HostListener('click') toogleOpen(){
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+
 
 }
