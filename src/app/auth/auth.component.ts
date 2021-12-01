50ac1528e4d6c451ce32a67627f54136af1a647d
MatteoLiberati <matteoliberati@hotmail.com>
2021-12-01 11:48:48 +0100

    return data of firebase api

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,7 +9,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
   isLoginMode : boolean = true;
 
   ngOnInit(): void {
@@ -19,7 +20,19 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form : NgForm){
-    console.log(form.value);
+    if(this.isLoginMode){
+
+    }else{
+      if(form.valid){
+        const email = form.value.email;
+        const password = form.value.password
+        this.authService.singUp(email, password).subscribe( resData=>{
+          console.log(resData);
+        }, error=>{
+          console.log(error);
+        });
+      }
+    }
     form.reset();
   }
 
