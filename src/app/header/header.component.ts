b0ef7f3c2d9d527c39e785c7f8bbaf5128ce825c
MatteoLiberati <matteoliberati@hotmail.com>
2021-12-14 11:03:54 +0100

    parts of header visible only when user is authenticated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,10 +8,19 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit, OnDestroy {
+  constructor(private dataStorageService : DataStorageService,
+              private authService : AuthService){}
 
-  constructor(private dataStorageService : DataStorageService){}
+  private subAuth : Subscription;
   collapsed = true;
+  isAuthenticated = false;
+
+  ngOnInit() {
+    this.subAuth = this.authService.user.subscribe(user=>{
+      this.isAuthenticated = !!user;
+    })
+  }
 
   onSaveData(){
     this.dataStorageService.storageData();
@@ -17,6 +28,10 @@ export class HeaderComponent{
 
   onFetchData(){
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+      this.subAuth.unsubscribe();
   }
 }
 
