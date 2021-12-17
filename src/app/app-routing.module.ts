436aa5ba1d7bc2595235c1a0bf8caa99a5ad838f
MatteoLiberati <matteoliberati@hotmail.com>
2021-12-17 15:32:49 +0100

    split routing recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth-guard.guard';
 import { AuthComponent } from './auth/auth.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeResolver } from './recipes/recipe-resolver.resolver';
-import { RecipesComponent } from './recipes/recipes.component';
-import { UnselectedItemComponent } from './recipes/unselected-item/unselected-item.component';
+import { RecipesRouting } from './recipes/recipes-routing.module';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const routes: Routes = [  
   {path: '', redirectTo: '/recipes', pathMatch: "full"},
   {path: 'auth', component: AuthComponent},
-  {path: 'recipes',
-   component: RecipesComponent, 
-   canActivate : [AuthGuard],
-   children: [
-    {path: '', component: UnselectedItemComponent},
-    {path: 'new', component: RecipeEditComponent},
-    {path: ':id', resolve: [RecipeResolver], component: RecipeDetailComponent},
-    {path: ':id/edit', resolve: [RecipeResolver], component: RecipeEditComponent},
-  ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,
+          RecipesRouting]
 })
 export class AppRoutingModule { }
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AuthGuard } from "../auth/auth-guard.guard";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeResolver } from "./recipe-resolver.resolver";
+import { RecipesComponent } from "./recipes.component";
+import { UnselectedItemComponent } from "./unselected-item/unselected-item.component";
+
+const recipesRoutes =
+    [{path: 'recipes',
+            component: RecipesComponent, 
+            canActivate : [AuthGuard],
+            children: [
+            {path: '', component: UnselectedItemComponent},
+            {path: 'new', component: RecipeEditComponent},
+            {path: ':id', resolve: [RecipeResolver], component: RecipeDetailComponent},
+            {path: ':id/edit', resolve: [RecipeResolver], component: RecipeEditComponent},
+        ]}]
+
+@NgModule({
+    imports: [RouterModule.forChild(recipesRoutes)],
+    exports: [RouterModule]
+})
+
+export class RecipesRouting {
+
+}
